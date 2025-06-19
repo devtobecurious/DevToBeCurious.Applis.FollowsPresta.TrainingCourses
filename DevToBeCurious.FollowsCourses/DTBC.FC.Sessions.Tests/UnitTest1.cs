917f43bd679857e7aeff61becc9bb9cd3eb09f21
Evan BOISSONNOT <evan@boissonnot.fr>
2025-06-19 15:16:24 +0200

    chore: prepare architecture for sessions

---
+++ DevToBeCurious.FollowsCourses/DTBC.FC.Sessions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace DTBC.FC.Sessions.Tests
+{
+    public class SessionsUnitTests
+    {
+        #region Public methods
+        [Fact]
+        public void ShouldCreateACompleteSession()
+        {
+
+        }
+        #endregion
+    }
+}
