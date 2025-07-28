f3456d9dc78436b0fbae1669f20d903f3106417b
Evan BOISSONNOT <evan@boissonnot.fr>
2025-07-28 23:00:32 +0200

    refacto: use new centers business

--- trainingcourses-workspace/projects/training-sessions/src/lib/components/add-new-session/add-new-session.ts
+++ trainingcourses-workspace/projects/training-sessions/src/lib/components/add-new-session/add-new-session.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { GET_ALL_URL } from 'dtbc-core';
 import { getAllRawTrainingCoursesFactory } from '../../services/factories/training-courses-services.factories';
-import { GetAllTrainingCentersBusiness } from '../../services/get-all-training-centers-business';
+import { GET_ALL_TRAINING_CENTERS_RAW, GetAllTrainingCentersBusiness } from '../../services/get-all-training-centers-business';
 import { GET_ALL_TRAINING_COURSES_RAW, GetAllTrainingCoursesBusiness } from '../../services/get-all-training-courses-business';
 import { SelectTrainingCenters } from '../select-training-centers/select-training-centers';
 import { SelectTrainingCourses } from '../select-training-courses/select-training-courses';
+import { getAllRawTrainingCentersFactory } from '../../services/factories/training-centers-services.factories';
 
 @Component({
   selector: 'lfpa-add-new-session',
@@ -14,8 +15,10 @@ import { SelectTrainingCourses } from '../select-training-courses/select-trainin
   styleUrl: './add-new-session.css',
   providers: [
     GetAllTrainingCoursesBusiness,
+    GetAllTrainingCentersBusiness,
     { provide: GET_ALL_URL, useValue: 'training-courses' },
-    { provide: GET_ALL_TRAINING_COURSES_RAW, useFactory: getAllRawTrainingCoursesFactory }
+    { provide: GET_ALL_TRAINING_COURSES_RAW, useFactory: getAllRawTrainingCoursesFactory },
+    { provide: GET_ALL_TRAINING_CENTERS_RAW, useFactory: getAllRawTrainingCentersFactory },
   ]
 })
 export class AddNewSession {
