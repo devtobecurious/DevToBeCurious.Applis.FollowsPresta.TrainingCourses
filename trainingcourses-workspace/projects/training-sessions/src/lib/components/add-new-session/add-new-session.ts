f07a313f7860b6f3ab75d4368cc7205613eda1b1
Evan BOISSONNOT <evan@boissonnot.fr>
2025-07-28 22:51:58 +0200

    review: remove all unused imports

--- trainingcourses-workspace/projects/training-sessions/src/lib/components/add-new-session/add-new-session.ts
+++ trainingcourses-workspace/projects/training-sessions/src/lib/components/add-new-session/add-new-session.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { GET_ALL_TRAINING_COURSES_RAW, GetAllTrainingCoursesBusiness } from '../../services/get-all-training-courses-business';
-import { SelectTrainingCourses } from '../select-training-courses/select-training-courses';
-import { GetAllTrainingCentersBusiness } from '../../services/get-all-training-centers-business';
-import { SelectTrainingCenters } from '../select-training-centers/select-training-centers';
-import { GetAllRawTrainingCourses } from '../../services/get-all-raw-training-courses';
 import { GET_ALL_URL } from 'dtbc-core';
-import { FakeGetAllTrainingCourses } from '../../services/mock/fake-get-all-training-courses';
 import { getAllRawTrainingCoursesFactory } from '../../services/factories/training-courses-services.factories';
+import { GetAllTrainingCentersBusiness } from '../../services/get-all-training-centers-business';
+import { GET_ALL_TRAINING_COURSES_RAW, GetAllTrainingCoursesBusiness } from '../../services/get-all-training-courses-business';
+import { SelectTrainingCenters } from '../select-training-centers/select-training-centers';
+import { SelectTrainingCourses } from '../select-training-courses/select-training-courses';
 
 @Component({
   selector: 'lfpa-add-new-session',
